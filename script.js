53d3e8e6c280d2db4cc5714f0459f7e38c0ed533
Holden Smith <105118057+LineZealot@users.noreply.github.com>
2023-05-28 09:42:53 -0500

    script.js gameboard rendering function

---
+++ script.js
@@ -0,0 +1,34 @@
+function playGame() {
+  const pageBody = document.querySelector('body');
+
+  const Gameboard = {
+    gameBoardSquares: [],
+    createGameBoard: () => {
+      const gameBoardTable = document.createElement('table');
+      gameBoardTable.className = 'game-board';
+
+      for (let i = 0; i < 3; i += 1) {
+        const gameRow = document.createElement('tr');
+        const rowSquareId = `row-${i}-`;
+        gameRow.className = 'game-row';
+        gameRow.id = `game-row${i}`;
+
+        for (let j = 0; j < 3; j += 1) {
+          const gameSquare = document.createElement('td');
+          gameSquare.className = 'game-square';
+          gameSquare.id = `${rowSquareId}square-${j}`;
+          gameRow.appendChild(gameSquare);
+          Gameboard.gameBoardSquares.push(gameSquare); // Use correct syntax to push to the array
+        }
+        gameBoardTable.appendChild(gameRow);
+      }
+
+      pageBody.appendChild(gameBoardTable);
+    },
+  };
+
+  Gameboard.createGameBoard();
+  console.log(Gameboard.gameBoardSquares);
+}
+
+playGame();
